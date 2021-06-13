7626c49a3c0c658b26a9b90dbe5808576ae32e80
Tomas Bankauskas <tbdizainas@gmail.com>
2021-06-13 10:15:36 +0300

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      colors:{
+        primary: {
+          DEFAULT: '#4801FF',
+          dark: '#3601bf',
+        },
+        secondary: {
+          DEFAULT: '#1F2937',
+          dark: '#171f29',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
